95fca1488bcb2ab82a6ddf126174b1266456b475
Florian Mounier <florian.mounier@kozea.fr>
2017-09-29 14:28:32 +0200

    Add delete fruits tests

--- unrest/tests/flask/test_delete.py
+++ unrest/tests/flask/test_delete.py
@@ -1,5 +1,5 @@
 from . import idsorted
-from ..model import Tree
+from ..model import Fruit, Tree
 
 
 def test_delete_tree_implicitly_unallowed(rest, http):
@@ -29,6 +29,49 @@ def test_delete_tree(rest, http):
     ]
 
     code, json = http.get('/api/tree')
+    assert code == 200
+    assert json['occurences'] == 0
+    assert idsorted(json['objects']) == []
+
+
+def test_delete_fruits(rest, http):
+    rest(Fruit, methods=['GET', 'DELETE'], allow_batch=True)
+    code, json = http.delete('/api/fruit')
+    assert code == 200
+    assert json['occurences'] == 5
+    assert idsorted(json['objects'], 'fruit_id') == [{
+        'fruit_id': 1,
+        'color': 'grey',
+        'size': 12.0,
+        'age': 1041300.0,
+        'tree_id': 1
+    }, {
+        'fruit_id': 2,
+        'color': 'darkgrey',
+        'size': 23.0,
+        'age': 4233830.213,
+        'tree_id': 1
+    }, {
+        'fruit_id': 3,
+        'color': 'brown',
+        'size': 2.12,
+        'age': 0.0,
+        'tree_id': 1
+    }, {
+        'fruit_id': 4,
+        'color': 'red',
+        'size': 0.5,
+        'age': 2400.0,
+        'tree_id': 2
+    }, {
+        'fruit_id': 5,
+        'color': 'orangered',
+        'size': 100.0,
+        'age': 7200.000012,
+        'tree_id': 2
+    }]
+
+    code, json = http.get('/api/fruit')
     assert code == 200
     assert json['occurences'] == 0
     assert idsorted(json['objects']) == []
--- unrest/tests/flask/test_delete_pk.py
+++ unrest/tests/flask/test_delete_pk.py
@@ -1,5 +1,5 @@
 from . import idsorted
-from ..model import Tree
+from ..model import Fruit, Tree
 
 
 def test_delete_tree(rest, http):
@@ -33,3 +33,48 @@ def test_delete_unknown_tree(rest, http):
     rest(Tree, methods=['DELETE'])
     code, json = http.delete('/api/tree/9')
     assert code == 404
+
+
+def test_delete_fruit(rest, http):
+    rest(Fruit, methods=['GET', 'DELETE'])
+    code, json = http.delete('/api/fruit/2')
+    assert code == 200
+    assert json['occurences'] == 1
+    assert idsorted(json['objects'], 'fruit_id') == [
+        {
+            'fruit_id': 2,
+            'color': 'darkgrey',
+            'size': 23.0,
+            'age': 4233830.213,
+            'tree_id': 1
+        },
+    ]
+
+    code, json = http.get('/api/fruit')
+    assert code == 200
+    assert json['occurences'] == 4
+    assert idsorted(json['objects'], 'fruit_id') == [{
+        'fruit_id': 1,
+        'color': 'grey',
+        'size': 12.0,
+        'age': 1041300.0,
+        'tree_id': 1
+    }, {
+        'fruit_id': 3,
+        'color': 'brown',
+        'size': 2.12,
+        'age': 0.0,
+        'tree_id': 1
+    }, {
+        'fruit_id': 4,
+        'color': 'red',
+        'size': 0.5,
+        'age': 2400.0,
+        'tree_id': 2
+    }, {
+        'fruit_id': 5,
+        'color': 'orangered',
+        'size': 100.0,
+        'age': 7200.000012,
+        'tree_id': 2
+    }]
